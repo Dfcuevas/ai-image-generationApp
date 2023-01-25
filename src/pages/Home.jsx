7e5f6f7d564d9a44a401f0ceaa145c1fb6ce36b0
Diego Cuevas <ingdcuevas@gmail.com>
2023-01-25 17:51:38 -0500

    agregando la url del servidor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,10 +23,13 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-        });
+        const response = await fetch(
+          "https://ai-image-generationapp-server-production.up.railway.app/api/v1/post",
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
